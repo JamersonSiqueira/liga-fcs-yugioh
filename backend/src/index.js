cb0f6baeb19c1b8ea532efbc4a56f58c1ae7963c
Jamerson Siqueira <jamerson.ss16@gmail.com>
2026-03-22 21:41:27 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,15 +18,7 @@ const allowedOrigins = [
 ]
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true)
-
-    if (allowedOrigins.includes(origin)) {
-      return callback(null, true)
-    }
-
-    return callback(new Error("Not allowed by CORS"))
-  }
+  origin: "*"
 }))
 app.use(express.json())
 
